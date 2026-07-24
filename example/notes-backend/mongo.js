8026e1bca809d9de91886cd3ef9f91fc42100fdd
willC1pher <kevstudyacc@gmail.com>
2026-07-24 09:57:53 +0700

    practice until 3.12

---
+++ example/notes-backend/mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = 
+    `mongodb+srv://willcipher:${password}@fullstackopen.trghile.mongodb.net/noteApp?retryWrites=true&w=majority&appName=FullStackOpen`
+
+mongoose.set('strictQuery',false)
+
+// Establish the connection to DB
+mongoose.connect(url, { family: 4 })    // {family: 4} = IPv4
+
+
+// Define schema
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+// const note = new Note({
+//   content: 'HTML is easy',
+//   important: true,
+// })
+
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
+
+// find({ condition }) => empty = all 
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
