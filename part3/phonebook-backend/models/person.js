b5ffd29443619eaaa07e70c5e538b3b05b361a84
willC1pher <kevstudyacc@gmail.com>
2026-07-30 07:58:27 +0700

    Done 3.19 - 3.20 and prepare for 3.21 production deployment

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -14,8 +14,25 @@ mongoose.connect(url, { family: 4 })
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        minLength: 3,
+    },
+    number: {
+        type: String,
+        validate: {
+            validator: function(num) {
+                // ^: Start of string
+                // $: End of string
+                // \d: One digit, \d{x, y}: x digits to y digits
+                // +: One digit or more
+                // .test(): Evaluate if a value matches the regex or not (Return True/False)
+                return /^\d{2,3}-\d+$/.test(num)
+            },
+            message: 'Phone number must be in the format XX-XXXXXXXX or XXX-XXXXXXX'
+        },
+        minLength: 8,
+    },
 })
 
 personSchema.set('toJSON', {
