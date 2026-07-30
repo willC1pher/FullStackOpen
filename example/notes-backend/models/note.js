32469635985c86b57385ec2fa60c8dfb79fb54f2
willC1pher <kevstudyacc@gmail.com>
2026-07-30 16:12:18 +0700

    part 4.1 example

--- example/notes-backend/models/note.js
+++ example/notes-backend/models/note.js
@@ -1,20 +1,11 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-mongoose.connect(url, { family: 4 })
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
-  content: String,
+  content: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
   important: Boolean,
 })
 
@@ -25,6 +16,5 @@ noteSchema.set('toJSON', {
     delete returnedObject.__v
   }
 })
-
 
 module.exports = mongoose.model('Note', noteSchema)
