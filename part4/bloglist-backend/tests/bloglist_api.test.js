6e0a38123180ea7cea87179e529e62fddad3081e
willC1pher <kevstudyacc@gmail.com>
2026-08-07 10:36:19 +0700

    done ex4.9 and 4.10

--- part4/bloglist-backend/tests/bloglist_api.test.js
+++ part4/bloglist-backend/tests/bloglist_api.test.js
@@ -4,33 +4,16 @@ const supertest = require('supertest')
 const app = require('../app')
 const assert = require('node:assert')
 const Blog = require('../models/blog')
+const helper = require('./blogtest_helper')
+const blog = require('../models/blog')
 
 const api = supertest(app)
 
-const initialBlogs = [
-	{
-		_id: '5a422a851b54a676234d17f7',
-		title: 'React patterns',
-		author: 'Michael Chan',
-		url: 'https://reactpatterns.com/',
-		likes: 7,
-		__v: 0
-	},
-	{
-		_id: '5a422aa71b54a676234d17f8',
-		title: 'Go To Statement Considered Harmful',
-		author: 'Edsger W. Dijkstra',
-		url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-		likes: 5,
-		__v: 0
-	}
-]
-
 beforeEach(async () => {
 	await Blog.deleteMany({})
-	let blogObject = new Blog(initialBlogs[0])
+	let blogObject = new Blog(helper.initialBlogs[0])
 	await blogObject.save()
-	blogObject = new Blog(initialBlogs[1])
+	blogObject = new Blog(helper.initialBlogs[1])
 	await blogObject.save()
 })
 
@@ -38,15 +21,59 @@ test('blogs are returned as json', async () => {
     await api
 			.get('/api/blogs')
 			.expect(200)
-			// Check that the response is in JSON format
 			.expect('Content-Type', /application\/json/)
 })
 
 test('all blogs are returned', async () => {
 	const response = await api.get('/api/blogs')
 
-	// Assure the test returns the correct number of blog posts
-	assert.strictEqual(response.body.length, initialBlogs.length)
+	assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+test('the unique identifier property of the blog posts is named id', async () => {
+	const response = await api.get('/api/blogs')
+
+	assert('id' in response.body[0])
+})
+
+test('a valid blog posts can be added', async () => {
+	const newBlog = {
+		title: 'A Valid Blog',
+		author: 'Frederick Ederson',
+		url: 'https://validblog.com/',
+		likes: 999,
+	}
+
+	await api
+		.post('/api/blogs')
+		.send(newBlog)
+		.expect(201)
+		.expect('Content-Type', /application\/json/)
+
+	const listAfterPost = await helper.blogsInDb()
+	assert.strictEqual(listAfterPost.length, helper.initialBlogs.length + 1)
+})
+
+test('the content of the uploaded blog post is saved correctly to the database', async () => {
+	const newBlog = {
+		title: 'A Valid Blog',
+		author: 'Frederick Ederson',
+		url: 'https://validblog.com/',
+		likes: 999,
+	}
+
+	await api
+		.post('/api/blogs')
+		.send(newBlog)
+		.expect(201)
+		.expect('Content-Type', /application\/json/)
+	
+	const response = await api.get('/api/blogs')
+	const savedBlog = response.body.find(blog => blog.title === newBlog.title)
+
+	assert.strictEqual(savedBlog.title, newBlog.title)
+	assert.strictEqual(savedBlog.author, newBlog.author)
+	assert.strictEqual(savedBlog.url, newBlog.url)
 })
 
 after(async () => {
