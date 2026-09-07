4e46c2b204790ab15033d0c339160daa2cc65c45
willC1pher <kevstudyacc@gmail.com>
2026-09-07 08:05:16 +0700

    Done ex 4.17

--- part4/bloglist-backend/controllers/users.js
+++ part4/bloglist-backend/controllers/users.js
@@ -1,9 +1,11 @@
 const bcrypt = require('bcryptjs')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
+const Blog = require('../models/blog')
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User
+    .find({}).populate('blogs')
   response.json(users)
 })
 
@@ -27,7 +29,7 @@ usersRouter.post('/', async (request, response) => {
   const user = new User({
     username,
     name,
-    passwordHash,
+    passwordHash
   })
 
   const savedUser = await user.save()
